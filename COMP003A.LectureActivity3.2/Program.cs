89018a54a7e2a8458aba1bcc025516d74ebefd1e
flores-97 <floresvictor15@gmail.com>
2025-01-24 14:16:34 -0800

    Add project files.

---
+++ COMP003A.LectureActivity3.2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Author: Victor Flores
+ Course: COMP-003A
+Faculty: Jonathon Cruz
+Purpose: Demonstrate selection statements in C#*/
+namespace COMP003A.LectureActivity3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
